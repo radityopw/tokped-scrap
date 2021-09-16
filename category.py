74a96a1fce257ec502ef63fe1656c1a9c8cd49db
radityopw <radityo_pw@is.its.ac.id>
2021-09-16 22:09:33 +0700

    membuat contoh untuk ekstrak kategori

--- category.py
+++ category.py
@@ -1,7 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-import requests
+import os
 
-r = requests.get('https://www.tokopedia.com/p')
-soup = BeautifulSoup(r.text, 'html.parser')
+# Instantiate options
+opts = Options()
+# opts.add_argument(" — headless") # Uncomment if the headless version needed
+opts.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
 
+# Set the location of the webdriver
+chrome_driver = r"C:\apps\chromedriver\chromedriver.exe"
 
+# Instantiate a webdriver
+driver = webdriver.Chrome(options=opts, executable_path=chrome_driver)
+
+# Load the HTML page
+driver.get("https://tokopedia.com/p/")
+
+# Parse processed webpage with BeautifulSoup
+soup = BeautifulSoup(driver.page_source)
+all_links = soup.find_all('a')
+
+driver.close()
+
+print(all_links)
