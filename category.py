ae1bf25e1c065bc53882138a2721f738c350f06d
radityopw <radityo_pw@is.its.ac.id>
2021-09-18 11:13:06 +0700

    menggunakan scrapingant.com

--- category.py
+++ category.py
@@ -1,26 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import os
+import config
+from scrapingant_client import ScrapingAntClient
+from os.path import exists
 
-# Instantiate options
-opts = Options()
-# opts.add_argument(" — headless") # Uncomment if the headless version needed
-opts.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+cache_file = config.cache_file("category")
 
-# Set the location of the webdriver
-chrome_driver = r"C:\apps\chromedriver\chromedriver.exe"
+if not exists(cache_file) :
 
-# Instantiate a webdriver
-driver = webdriver.Chrome(options=opts, executable_path=chrome_driver)
 
-# Load the HTML page
-driver.get("https://tokopedia.com/p/")
+    client = ScrapingAntClient(token='b3fac55e1b354ad0be818ba9c80248bd')
+    result = client.general_request('https://tokopedia.com/p/')
+    content = result.content
 
-# Parse processed webpage with BeautifulSoup
-soup = BeautifulSoup(driver.page_source)
+    with open(cache_file,'w', encoding = 'utf-8') as f :
+        f.write(content)
+
+with open(cache_file,'r', encoding = 'utf-8') as f :
+    content = f.read()
+
+
+soup = BeautifulSoup(content,features="html.parser")
 all_links = soup.find_all('a')
 
-driver.close()
 
-print(all_links)
+for link in all_links:
+    print(link)
+    print(link['href'])
+    print(link.contents[0])
---
+++ config.py
@@ -0,0 +1,5 @@
+def dir_location():
+    return "/tmp"
+
+def cache_file(filename):
+    return dir_location()+"/tokped_scrap_"+filename
