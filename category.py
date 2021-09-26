e5908e928e76b4f8abf7f6e2acf858948ddf6b6d
radityopw <radityo_pw@is.its.ac.id>
2021-09-26 19:56:08 +0700

    adding exception

--- category.py
+++ category.py
@@ -9,152 +9,165 @@ import scrap
 
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
-
-
 logging.info("starting category")
 
 cache_file = config.cache_file("category")
 db_file = config.db_file("tokped")
 url = "https://tokopedia.com/p/"
 
-if not exists(cache_file) :
-    scrap.scrap_this(url,cache_file)
-    
-with open(cache_file,'r', encoding = 'utf-8') as f :
-    content = f.read()
-
-
-soup = BeautifulSoup(content,features="html.parser")
-all_links = soup.find_all('a')
 
 con = sqlite3.connect(db_file)
-cur = con.cursor()
 
-sql = """
-        CREATE TABLE IF NOT EXISTS category (
-             id INTEGER PRIMARY KEY AUTOINCREMENT
-            ,title TEXT COLLATE NOCASE NULL
-            ,url TEXT COLLATE NOCASE
-        );
-      """
+try : 
 
-cur.execute(sql)
-
-sql = "CREATE UNIQUE INDEX IF NOT EXISTS IX_url ON category(url);"
-
-cur.execute(sql)
+    if not exists(cache_file) :
+        scrap.scrap_this(url,cache_file)
+        
+    with open(cache_file,'r', encoding = 'utf-8') as f :
+        content = f.read()
 
 
+    soup = BeautifulSoup(content,features="html.parser")
+    all_links = soup.find_all('a')
 
-sql = """
-        CREATE TABLE IF NOT EXISTS category_level (
-             category_id INTEGER
-            ,level INTEGER 
-            ,title TEXT COLLATE NOCASE
-            ,PRIMARY KEY(category_id,level)
-            ,FOREIGN KEY(category_id) REFERENCES category(id)
-        );
-      """
 
-cur.execute(sql)
+    cur = con.cursor()
 
-sql = """
-        CREATE TABLE IF NOT EXISTS category_level_max (
-             category_id INTEGER
-            ,max_level INTEGER
-            ,PRIMARY KEY(category_id,max_level)
-            ,FOREIGN KEY(category_id) REFERENCES category(id)
-        );
-      """
-cur.execute(sql)
+    sql = """
+            CREATE TABLE IF NOT EXISTS category (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT
+                ,title TEXT COLLATE NOCASE NULL
+                ,url TEXT COLLATE NOCASE
+            );
+          """
+
+    cur.execute(sql)
+
+    sql = "CREATE UNIQUE INDEX IF NOT EXISTS IX_url ON category(url);"
+
+    cur.execute(sql)
 
 
 
+    sql = """
+            CREATE TABLE IF NOT EXISTS category_level (
+                 category_id INTEGER
+                ,level INTEGER 
+                ,title TEXT COLLATE NOCASE
+                ,PRIMARY KEY(category_id,level)
+                ,FOREIGN KEY(category_id) REFERENCES category(id)
+            );
+          """
 
-for link in all_links:
-    href = str(link['href']).strip().lower()
-    title = str(link.contents[0]).strip().lower()
-    header_link = "https://www.tokopedia.com/p/" 
+    cur.execute(sql)
 
-    if href is not None and header_link in href  :
-
-        href_short = href.replace(header_link,"")
-
-        href_level = href_short.split("/")
-
-        logging.info("process "+href+" and title "+title)
-
-        logging.info("href level : "+str(href_level))
-
-
-        sql = f"""
-                SELECT id 
-                FROM category
-                WHERE url = ?
-              """
-
-        logging.debug(sql)
-
-        row = cur.execute(sql,(href,)).fetchone()
-
-        if row is None :
-
-            logging.debug("insert mode")
+    sql = """
+            CREATE TABLE IF NOT EXISTS category_level_max (
+                 category_id INTEGER
+                ,max_level INTEGER
+                ,PRIMARY KEY(category_id,max_level)
+                ,FOREIGN KEY(category_id) REFERENCES category(id)
+            );
+          """
+    cur.execute(sql)
 
 
-            sql = f""" 
-                    INSERT INTO category(title,url)
-                    VALUES(?,?);
-                """
-            cur.execute(sql,(title,href))
 
-            id = cur.lastrowid
 
-        else :
+    for link in all_links:
+        href = str(link['href']).strip().lower()
+        try :
+            title = str(link.contents[0]).strip().lower()
+        except Exception as e :
+            continue 
+        header_link = "https://www.tokopedia.com/p/" 
 
-            logging.debug("update mode")
+        if href is not None and header_link in href  :
 
-            id = row[0] # baris pertama , kolom id -> lihat query row
+            href_short = href.replace(header_link,"")
+
+            href_level = href_short.split("/")
+
+            logging.info("process "+href+" and title "+title)
+
+            logging.info("href level : "+str(href_level))
+
+
+            sql = f"""
+                    SELECT id 
+                    FROM category
+                    WHERE url = ?
+                  """
+
+            logging.debug(sql)
+
+            row = cur.execute(sql,(href,)).fetchone()
+
+            if row is None :
+
+                logging.debug("insert mode")
+
+
+                sql = f""" 
+                        INSERT INTO category(title,url)
+                        VALUES(?,?);
+                    """
+                cur.execute(sql,(title,href))
+
+                id = cur.lastrowid
+
+            else :
+
+                logging.debug("update mode")
+
+                id = row[0] # baris pertama , kolom id -> lihat query row
+
+                sql = """
+                        UPDATE category
+                        SET title = ?
+                        WHERE id = ?
+                      """
+                cur.execute(sql,(title,id))
 
             sql = """
-                    UPDATE category
-                    SET title = ?
-                    WHERE id = ?
+                    DELETE FROM category_level 
+                    WHERE category_id = ?
                   """
-            cur.execute(sql,(title,id))
+            cur.execute(sql,(id,))
 
-        sql = """
-                DELETE FROM category_level 
-                WHERE category_id = ?
-              """
-        cur.execute(sql,(id,))
+            level = 1
 
-        level = 1
+            for level_title in href_level :
 
-        for level_title in href_level :
+                sql = """
+                        INSERT INTO category_level(category_id,level,title)
+                        VALUES(?,?,?)
+                      """
+                cur.execute(sql,(id,level,level_title))
 
-            sql = """
-                    INSERT INTO category_level(category_id,level,title)
-                    VALUES(?,?,?)
-                  """
-            cur.execute(sql,(id,level,level_title))
-
-            level = level + 1
+                level = level + 1
 
 
-sql = "DELETE FROM category_level_max;"
-cur.execute(sql)
+    sql = "DELETE FROM category_level_max;"
+    cur.execute(sql)
 
-sql = """
-        INSERT INTO category_level_max(category_id,max_level)
-        SELECT category_id,max(level)
-        FROM category_level
-        GROUP BY category_id;
-      """
-cur.execute(sql)
+    sql = """
+            INSERT INTO category_level_max(category_id,max_level)
+            SELECT category_id,max(level)
+            FROM category_level
+            GROUP BY category_id;
+          """
+    cur.execute(sql)
 
 
 
-con.commit()
+    con.commit()
+    
+except Exception as ex :
+    
+    logging.error(ex)
+    
+    con.rollback()
 
-con.close()
+finally :
+    con.close()
--- list_page.py
+++ list_page.py
@@ -17,26 +17,108 @@ db_file = config.db_file("tokped")
   
 
 con = sqlite3.connect(db_file)
-cur = con.cursor()
 
 
-sql = "SELECT id,url FROM category"
-list_category = cur.execute(sql).fetchall()
+try :
 
-for category in list_category :
-    id = category[0]
-    url = category[1]
-    cache_file = config.cache_file("list_page_"+str(id))
+    cur = con.cursor()
 
-    if not exists(cache_file) :
-        scrap.scrap_this(url,cache_file)
-   
-    #with open(cache_file,'r', encoding = 'utf-8') as f :
-    #    content = f.read()
+    sql = """
+            CREATE TABLE IF NOT EXISTS list_page (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT
+                ,category_id INTEGER
+                ,title TEXT COLLATE NOCASE NULL
+                ,url TEXT COLLATE NOCASE
+                ,FOREIGN KEY(category_id) REFERENCES category(id)
+            );
+          """
+          
+    cur.execute(sql)
+
+    sql = """
+            CREATE UNIQUE INDEX IF NOT EXISTS IX_url ON list_page(url);
+          """
+          
+    cur.execute(sql)
 
 
+    sql = "SELECT id,url FROM category"
+    list_category = cur.execute(sql).fetchall()
 
+    for category in list_category :
+        category_id = category[0]
+        category_url = category[1]
+        cache_file = config.cache_file("list_page_"+str(category_id))
+        
+        logging.info("scrapping id "+str(category_id)+" "+category_url)
 
-con.commit()
+        content = ""
 
-con.close()
+        if not exists(cache_file) :
+            scrap.scrap_this(category_url,cache_file)
+       
+        with open(cache_file,'r', encoding = 'utf-8') as f :
+            content = f.read()
+            
+        soup = BeautifulSoup(content,features="html.parser")
+        all_links = soup.find_all('a')
+        
+        for link in all_links:
+        
+            #logging.debug(link)
+            
+            href = str(link['href']).strip().lower()
+            
+            try :
+                title = str(link.contents[0]).strip().lower()
+            except Exception as e :
+                continue
+            
+            if href is not None and "?whid=0" in href :
+            
+                href = href.split("?")[0]
+                
+                logging.debug("process "+href)
+            
+                sql = """
+                        SELECT id
+                        FROM list_page
+                        WHERE url = ?
+                      """
+                row = cur.execute(sql,(href,)).fetchone()
+                
+                if row is None :
+                    logging.debug("INSERT MODE")
+                    
+                    sql = """
+                            INSERT INTO list_page(category_id,title,url)
+                            VALUES(?,?,?)
+                          """
+                    
+                    cur.execute(sql,(category_id,title,href))
+                    
+                    id = cur.lastrowid
+                    
+                else : 
+                    logging.debug("UPDATE MODE")
+                    
+                    id = row[0]
+                    
+                    
+                    sql = """
+                            UPDATE list_page
+                            SET title = ?
+                            WHERE id = ?
+                          """
+                    
+                    cur.execute(sql,(title,id))
+                
+
+    con.commit()
+
+except Exception as ex1 :
+    logging.error(ex1)
+    con.rollback()
+
+finally :
+    con.close()
